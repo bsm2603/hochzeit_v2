d82d3c4a16914b31196465440bda02ae49fc5f45
Burkhart Salzmann <b.salzmann@mailbox.org>
2024-08-21 02:01:41 +0200

    Alle Tischrätselseiten fertig

--- t/282710.php
+++ t/282710.php
@@ -1,3 +1,74 @@
 <?php require_once('./head.php'); ?>
 
+<div class="popupcover" id="popupcover" onClick="closeAll()"></div>
+<div class="popup" id="hint">
+    <h2>Hinweis</h2>
+    <p>Statt das Labyrinth zu lösen, kannst Du auch gucken, welche von den 8 Tischnummern es wirklich gibt.</p>
+    <input type="button" value="OK" onclick="togglePopup('hint', 'none')" />
+</div>
+
+<div class="popup" id="guess">
+    <h2>Deine Lösung prüfen</h2>
+    <p>Gib Deine Lösung ein</p>
+    <input id="guessText" onKeyUp="resetGuessResult() "/>
+    <div id="guessCorrect">
+        <p>Das ist korrekt! Gratulation.</p>
+        <p><strong>Genieß die Party an, um oder auf Tisch 49.</strong></p>
+    </div>
+    <p id="guessWrong">Das ist leider noch nicht korrekt. Checke noch mal den Hinweis und rechne nach.</p>
+    <input type="button" value="OK" onclick="checkSolution(49)" />
+</div>
+
+<div class="popup" id="tell">
+    <h2>Die korrekte Lösung</h2>
+    <p>Der einzige Weg raus führt zur 49. Das ist auch der einzige Tisch im Raum der auf 9 endet.</p>
+    <p><strong>Deine Tischnummer ist die 49.</strong></p>
+    <input type="button" value="OK" onclick="togglePopup('tell', 'none')" />
+</div>
+
+<section id="page">
+    <div class="container">
+        <div class="heading" style="padding-bottom: 10px" data-scroll-reveal>
+            <h2>Tischrätsel Kid & Teens (Jungs)</h2>
+            <p><span></span><i class="fa fa-question"></i><span></span></p>
+        </div>
+    </div>
+</section>
+
+<table style="margin:0px; max-width: 400px; margin: auto">
+    <tbody>
+    <tr>
+        <td colspan="2" style="text-align: center;">Ich zeige Dir Deinen Platz.</td>
+    </tr>
+    <tr>
+        <td colspan="2" style="text-align: center;">&nbsp;</td>
+    </tr>
+    <tr>
+        <td style="text-align: center;" width="50%">
+            Collin<br />
+            Milo<br />
+            Emil<br />
+            Levi<br />
+            Mattis
+        </td>
+        <td style="text-align: center;" width="50%">
+            Hendrik<br />
+            Felix<br />
+            Tyler<br />
+            Julian<br />
+            Marin
+        </td>
+    </tr>
+    </tbody>
+</table>
+<p style="margin-top: 20px; text-align:center"><strong>So findest Du Deine Tischnummer:</strong><br/>
+    Du startest beim Pfeil in der Mitte des Labyrinths.<br />Es gelingt Dir, erfolgreich zu fliehen. Bei welcher Zahl kommst raus?<br />
+    <img src="labyrinth.jpg" width="300px" />
+</p>
+<form class="knopf-container" style="text-align:center">
+    <button type="button" onclick="togglePopup('hint', 'block')">Hinweis</button><br/>
+    <button type="button" onClick="togglePopup('guess', 'block')">Meine Lösung prüfen</button><br/>
+    <button type="button" onClick="togglePopup('tell', 'block')">Lösung anzeigen</button>
+</form>
+
 <?php require_once('./foot.php'); ?>
